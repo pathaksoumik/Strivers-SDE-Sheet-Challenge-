149355b33a4e030c734f91516873efdd7e86746d
X_Y_Z <55338475+pathaksoumik@users.noreply.github.com>
2023-07-18 22:17:20 +0530

    Create 1. Set Matrix Zeros.cpp

---
+++ 1. Set Matrix Zeros.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+	int colm=1;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++ )
+        {
+            if(matrix[i][0]==0)colm=0;
+
+            for(int j=1;j<col;j++ )
+            {
+                if(matrix[i][j]==0)
+                    matrix[i][0]=matrix[0][j]=0;
+             }
+        }
+
+        for(int i=row-1;i>=0;i--)
+        {
+
+            for(int j=col-1;j>=1;j--)
+            {
+                if( matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0; 
+            }
+            if(colm==0)matrix[i][0]=0;
+        }
+}
