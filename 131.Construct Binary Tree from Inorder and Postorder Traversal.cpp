218e9472c5d11065c9e342b3db605dd072c3d5bc
X_Y_Z <55338475+pathaksoumik@users.noreply.github.com>
2023-07-19 13:51:27 +0530

    Create 131.Construct Binary Tree from Inorder and Postorder Traversal.cpp

---
+++ 131.Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,65 @@
+/************************************************************
+   
+   Following is the TreeNode class structure
+   
+   class TreeNode<T>
+   { 
+   public:
+        T data; 
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+   
+        TreeNode(T data) 
+  		{ 
+            this -> data = data; 
+            left = NULL; 
+            right = NULL; 
+        }
+   };
+   
+   
+ ************************************************************/
+
+TreeNode<int>* buildTreeHelper(vector<int>& postOrder, int postStart, int postEnd, vector<int>& inOrder, int inStart, int inEnd) 
+{
+	if (postStart > postEnd || inStart > inEnd) 
+		return NULL;
+
+    // Assign rootVal as postOrder[postEnd]
+	int rootVal = postOrder[postEnd];
+    
+	TreeNode<int>* root = new TreeNode<int>(rootVal);
+
+	// Find parent element index from inOrder array.
+	int k = 0;
+    
+    // Iterate i from inStart to inEnd
+    for (int i = inStart; i <= inEnd; i++) 
+	{
+		if (rootVal == inOrder[i]) 
+		{
+			k = i;
+			break;
+		}
+	}
+
+    // Create recursive call for left and right subtree
+	root -> left = buildTreeHelper(postOrder, postStart, postStart + k - inStart - 1, inOrder, inStart, k - 1);
+	root -> right = buildTreeHelper(postOrder, postStart + k - inStart, postEnd - 1, inOrder, k + 1, inEnd);
+
+	return root;
+}
+
+TreeNode<int>* getTreeFromPostorderAndInorder(vector<int>& postOrder, vector<int>& inOrder) 
+{
+	int n = postOrder.size();
+
+	int postStart = 0;
+	int postEnd = n - 1;
+	int inStart = 0;
+	int inEnd = n - 1;
+
+    // Call buildTreeHelper function 
+	return buildTreeHelper(postOrder, postStart, postEnd, inOrder, inStart, inEnd);
+}
+
