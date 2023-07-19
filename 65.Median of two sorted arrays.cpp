a1bae4228b250df9f007f2216952b9439304affa
X_Y_Z <55338475+pathaksoumik@users.noreply.github.com>
2023-07-19 12:49:54 +0530

    Create 65.Median of two sorted arrays.cpp

---
+++ 65.Median of two sorted arrays.cpp
@@ -0,0 +1,29 @@
+double median(vector<int>& a, vector<int>& b) {
+	// Write your code here.
+	int m=a.size(),n=b.size();
+	
+	if(m>n)
+        return median(b,a);//ensuring that binary search happens on minimum size array
+        
+    int low=0,high=m,medianPos=((m+n)+1)/2;
+	
+    while(low<=high) {
+        int cut1 = low+((high-low)>>1);
+        int cut2 = medianPos - cut1;
+        
+        int l1 = (cut1 == 0)? INT_MIN:a[cut1-1];
+        int l2 = (cut2 == 0)? INT_MIN:b[cut2-1];
+        int r1 = (cut1 == m)? INT_MAX:a[cut1];
+        int r2 = (cut2 == n)? INT_MAX:b[cut2];
+        
+        if(l1<=r2 && l2<=r1) {
+            if((m+n)%2 != 0)
+                return max(l1,l2);
+            else 
+                return (max(l1,l2)+min(r1,r2))/2.0;
+        }
+        else if(l1>r2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
+}
