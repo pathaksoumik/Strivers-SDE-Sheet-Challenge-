f651e9ba5fb18581199cfd87920d2433057b53ab
X_Y_Z <55338475+pathaksoumik@users.noreply.github.com>
2023-07-19 14:26:55 +0530

    Create 176.Maximum Sum Increasing Subsequence.cpp

---
+++ 176.Maximum Sum Increasing Subsequence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+int maxIncreasingDumbbellsSum(vector<int> &rack, int n)
+{
+    int dp[n];
+    int totalWeight = INT_MIN;
+
+    for(int i = 0; i < n; i++)
+        dp[i] = rack[i];
+    
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            // Choose element which is smaller than last element.
+            if(rack[i] > rack[j] && dp[i] < dp[j] + rack[i])
+                dp[i] = dp[j] + rack[i];
+        }
+        totalWeight = max(totalWeight, dp[i]);
+    }
+
+    return totalWeight;
+}
